f37555b9e7f24a91ec5fc8de0133a065a8d2e63a
KoriSama <korisama35@gmail.com>
2025-04-16 03:45:59 +0800

    feat: finish predict api

--- router/predict.py
+++ router/predict.py
@@ -72,7 +72,29 @@ async def predict_api_v2(
             traffic_volume=item.traffic_volume
         ) for item in road_flows
     ]
-    outputs = predict.run(inputs)
+    # outputs = predict.run(inputs)
+    inputs.sort(key=lambda x: x.start_time)
+    idx = 0
+    predicts = []
+    while True:
+        if idx + 6 > len(inputs):
+            predicts += predict.run(inputs[-6:])
+            break
+
+        predicts += predict.run(inputs[idx:idx + 6])[:5]
+        idx += 6
+
+    # 去除predicts中时间相同的元素
+    seen_times = set()
+    unique_predicts = []
+    for item in predicts:
+        if item["time"] not in seen_times:
+            unique_predicts.append(item)
+            seen_times.add(item["time"])
+
+    predicts = unique_predicts
+    predicts.sort(key=lambda x: x["time"])
+
     return Ok(dict(
         last=[PredictOutput(
             time=item.start_time,
@@ -81,5 +103,5 @@ async def predict_api_v2(
         predict=[PredictOutput(
             time=item["time"],
             volume=item["predicted_traffic_volume"]
-        ) for item in outputs]
+        ) for item in predicts]
     ))
