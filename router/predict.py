8f7957102bbe430b5cd527b3b5db2dff2e0f3651
KoriSama <korisama35@gmail.com>
2025-04-16 15:11:16 +0800

    feat: add speed predict

--- router/predict.py
+++ router/predict.py
@@ -105,3 +105,65 @@ async def predict_api_v2(
             volume=item["predicted_traffic_volume"]
         ) for item in predicts]
     ))
+
+
+@router.post("/speed/lstm")
+async def predict_api_v2(
+    input_data: PredictInputV2
+) -> Response[PredictResponse]:
+    """
+    Predict traffic volume by LSTM model.
+    """
+
+    road_flows = await list_trunk_road_flows(
+        from_gantry_id=input_data.from_gantry_id,
+        to_gantry_id=input_data.to_gantry_id,
+        start_time=input_data.start_time,
+        end_time=input_data.end_time)
+
+    if not road_flows:
+        return Ok(dict(
+            last=[],
+            predict=[]
+        ))
+    inputs = [
+        predict.PredictInputData(
+            road_id=item.from_gantry_id,
+            start_time=item.start_time,
+            end_time=item.end_time,
+            traffic_volume=item.avg_speed
+        ) for item in road_flows
+    ]
+    # outputs = predict.run(inputs)
+    inputs.sort(key=lambda x: x.start_time)
+    idx = 0
+    predicts = []
+    while True:
+        if idx + 6 > len(inputs):
+            predicts += predict.run(inputs[-6:])
+            break
+
+        predicts += predict.run(inputs[idx:idx + 6])[:5]
+        idx += 6
+
+    # 去除predicts中时间相同的元素
+    seen_times = set()
+    unique_predicts = []
+    for item in predicts:
+        if item["time"] not in seen_times:
+            unique_predicts.append(item)
+            seen_times.add(item["time"])
+
+    predicts = unique_predicts
+    predicts.sort(key=lambda x: x["time"])
+
+    return Ok(dict(
+        last=[PredictOutput(
+            time=item.start_time,
+            volume=item.traffic_volume
+        ) for item in inputs],
+        predict=[PredictOutput(
+            time=item["time"],
+            volume=item["predicted_traffic_volume"]
+        ) for item in predicts]
+    ))
